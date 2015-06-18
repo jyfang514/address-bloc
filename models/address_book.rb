d264c7b3a92e1bbd8ecbcca4d9c3f5bdb936e48d
John Fang <jyfang514@gmail.com>
2015-06-18 21:14:12 +0700

    cp24 assignment complete

--- models/address_book.rb
+++ models/address_book.rb
@@ -17,32 +17,44 @@ class AddressBook
       index += 1
     end
     @entries.insert(index, Entry.new(name, phone, email))
-   end
+  end
 
-   def import_from_csv(file_name)
-     csv_text = File.read(file_name)
-     csv = CSV.parse(csv_text, headers: true)
-     csv.each do |row|
-       row_hash = row.to_hash
-       add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
+  def import_from_csv(file_name)
+   csv_text = File.read(file_name)
+   csv = CSV.parse(csv_text, headers: true)
+   csv.each do |row|
+     row_hash = row.to_hash
+     add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
+   end
+  end
+
+  def binary_search(name)
+   lower = 0
+   upper = entries.length - 1
+   while lower <= upper
+     mid = (lower + upper) / 2
+     mid_name = entries[mid].name
+     if name == mid_name
+       return entries[mid]
+     elsif name < mid_name
+       upper = mid - 1
+     elsif name > mid_name
+       lower = mid + 1
      end
    end
+   return nil
+  end
 
-   def binary_search(name)
-     lower = 0
-     upper = entries.length - 1
-     while lower <= upper
-       mid = (lower + upper) / 2
-       mid_name = entries[mid].name
-       if name == mid_name
-         return entries[mid]
-       elsif name < mid_name
-         upper = mid - 1
-       elsif name > mid_name
-         lower = mid + 1
-       end
+  def iterative_search(target_name)
+   lower = 0
+   upper = entries.length - 1
+   while lower <= upper
+     if target_name == entries[lower].name
+       return entries[lower]
+     else lower += 1
      end
-     return nil
    end
+   return nil
+  end
 
 end
